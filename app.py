2ac9934251ed9ff3e1d416784a6551ae0ba1a5fe
tauhidalam <tauhidalam1998@gmail.com>
2024-07-25 21:34:55 +0530

    Ensure database migrations are in place

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ import base64
 import seaborn as sns
 import pandas as pd
 from flask_migrate import Migrate
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///expense_tracker.db')
